f485b077bcb20bb9d94d76627e6f27dca51df898
Marc Sauter <sauterm6800@gmail.com>
2024-05-24 17:03:09 +0200

    Added .config/ folder

---
+++ .config/hypr/scripts/initialize_wallpaper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export PATH="${PATH}:${HOME}/.local/bin/"
+
+
+if [ -e "${HOME}/.cache/wal/colors" ]; then
+	wal -R
+else
+	DIR=$HOME/wallpaper
+	PICS=($(ls ${DIR}))
+	echo $PICS
+
+	RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+	swww img ${DIR}/${RANDOMPICS} --transition-type grow --transition-fps 60 --transition-duration 0.2 --transition-pos 0.810,0.972 --transition-bezier 0.65,0,0.35,1 --transition-step 255
+	wal -q -i ${DIR}/${RANDOMPICS}
+
+fi
